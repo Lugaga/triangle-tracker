c68be0be973f18b306d79143e2f86ab8897f1358
Lugaga <maureezgaga@gmail.com>
2019-04-28 13:21:25 +0300

    add descriptive comments to js/triangle-tracker.js

--- js/triangle-tracker.js
+++ js/triangle-tracker.js
@@ -1,23 +1,27 @@
+//create the function triangleTracker
 var triangleTracker = function (sidenumberone, sidenumbertwo, sidenumberthree) {
     var sidenumberone = parseInt(document.getElementById("a").value);
     var sidenumbertwo = parseInt(document.getElementById("b").value);
     var sidenumberthree = parseInt(document.getElementById("c").value);
 
+// create variable triangleSides
     var triangleSides = [];
     triangleSides.push(sidenumberone);
     triangleSides.push(sidenumbertwo);
     triangleSides.push(sidenumberthree);
 
+// Initialize the control flow (loop)
     if (
         sidenumberone + sidenumbertwo > sidenumberthree &&
         sidenumberone + sidenumberthree > sidenumbertwo &&
         sidenumbertwo + sidenumberthree > sidenumberone &&
         (sidenumberone === sidenumbertwo && sidenumbertwo === sidenumberthree)
     ) {
+// Output Equilateral Triangle       
         document.getElementById("output").innerHTML =
         "It is an Equilateral Triangle with side values of<br> " + triangleSides + "";
         document.getElementById("triangle-image").src = "./img/equilateral-triangle.png";
-
+// Output Isosceles Triangle
     } else if (
         sidenumberone + sidenumbertwo > sidenumberthree &&
         sidenumberone + sidenumberthree > sidenumbertwo &&
@@ -31,6 +35,7 @@ var triangleTracker = function (sidenumberone, sidenumbertwo, sidenumberthree) {
         "It is an Isosceles Triangle with side values of <br> " + triangleSides + "";
         document.getElementById("triangle-image").src = "./img/isosceles-triangle.png";
 
+// Output Scalene Triangle
     } else if (
         sidenumberone + sidenumbertwo > sidenumberthree &&
         sidenumberone + sidenumberthree > sidenumbertwo &&
@@ -41,12 +46,12 @@ var triangleTracker = function (sidenumberone, sidenumbertwo, sidenumberthree) {
         document.getElementById("output").innerHTML =
         "It is a Scalene Triangle with side values of <br> " + triangleSides + "";
         document.getElementById("triangle-image").src = "./img/scalene-triangle.png";
-
+// Null
     } else if (!sidenumberone || !sidenumbertwo || !sidenumberthree) {
         document.getElementById("output").innerHTML =
         "Kindly enter all the triangle values and try again!";
         
-
+// Invalid values output
     } else {
         document.getElementById("output").innerHTML = " Invalid Values, Not a Triangle at all !";
         document.getElementById("triangle-image").src = "./img/hazard.png";
