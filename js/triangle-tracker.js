78d0a1eb6562718d07357e0917cdb740650662f6
Lugaga <maureezgaga@gmail.com>
2019-04-28 12:32:44 +0300

    add triangleSides Variable to custom javascript file

--- js/triangle-tracker.js
+++ js/triangle-tracker.js
@@ -2,3 +2,8 @@ var triangleTracker = function (sidenumberone, sidenumbertwo, sidenumberthree) {
     var sidenumberone = parseInt(document.getElementById("a").value);
     var sidenumbertwo = parseInt(document.getElementById("b").value);
     var sidenumberthree = parseInt(document.getElementById("c").value);
+
+    var triangleSides = [];
+    triangleSides.push(sidenumberone);
+    triangleSides.push(sidenumbertwo);
+    triangleSides.push(sidenumberthree);
